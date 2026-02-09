75425461d409401b4bf625edda2c6e38028c7335
Jeyong <dlwpdyd201@naver.com>
2026-02-10 06:12:16 +0900

    fix: lambda error 수정

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,10 +1,11 @@
-import { createLambdaFunction, createProbot } from "@probot/adapter-aws-lambda-serverless";
+import adapter from "@probot/adapter-aws-lambda-serverless";
 import app from "./index.js";
 
 if (!process.env.PRIVATE_KEY && process.env.PRIVATE_KEY_BASE64) {
   process.env.PRIVATE_KEY = Buffer.from(process.env.PRIVATE_KEY_BASE64, "base64").toString("utf-8");
 }
 
+const { createLambdaFunction, createProbot } = adapter;
 const probot = createProbot();
 const lambdaApp = app as unknown as Parameters<typeof createLambdaFunction>[0];
 
